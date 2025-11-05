f5271b8da7288b04c702105551b048597d64a03a
Sri Charan Byreddy <145511991+Sricharanbyreddy9@users.noreply.github.com>
2025-11-05 12:01:23 -0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -127,7 +127,7 @@ const Hero = () => {
           <Button
             variant="neon"
             size="lg"
-            onClick={() => window.open("https://drive.google.com/file/d/1xWfQI8PTiEZzPOUtCGS1DwPMxkRWlc-V/view?usp=sharing", "_blank")}
+            onClick={() => window.open("https://drive.google.com/file/d/1TWUcWsvC6NjKA2Q0LYqkmwKeH1mZBKQC/view?usp=sharing", "_blank")}
             className="gap-2"
           >
             <Download className="w-5 h-5" />
